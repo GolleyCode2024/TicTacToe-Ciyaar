30760b65406b016a783a5e614d73ffc401717cef
GolleyCode <golleycode@gmail.com>
2024-09-12 11:10:18 +0200

    Waxaan sameeyey isbedel badan iyo so aan Game u hor mariyo anigoo ku soo darey X iyo 0

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,49 @@
+import { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleSquareClick = clickPosition => {
+    if (squares[clickPosition]) {
+      return;
+    }
+    setSquares(currentSquare => {
+      return currentSquare.map((squareValue, position) => {
+        if (clickPosition === position) {
+          return isXNext ? 'X' : '0';
+        }
+        return squareValue;
+      });
+    });
+    setIsXNext(prevCurrent => !prevCurrent);
+  };
+
+  const renderSquare = clickPosition => {
+    return (
+      <Square
+        value={squares[clickPosition]}
+        onClick={() => handleSquareClick(clickPosition)}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
